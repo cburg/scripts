b8f05b6ccd57bc4d135bcb2360a6163268300c9b
Chris Burgess (AZURE) <chburges@microsoft.com>
2018-10-28 15:10:45 -0700

    Rewrite new install setup script

--- ubuntu_new_install.sh
+++ ubuntu_new_install.sh
@@ -1,117 +1,252 @@
 #!/bin/bash
 
-# Simple script to automate a fresh Ubuntu install. Aside from some initial
-# passwords and menus, it should for the most part be automated. 
+SCRIPTROOT="$( dirname "${BASH_SOURCE[0]}" )"
+
+#####################################################################
+# Development Packages
+#####################################################################
+DEVELOPMENT_PACKAGES=(
+    # Applications
+    vim
+    emacs
+    git
+    make
+    autoconf
+    automake
+    exuberant-ctags
+    screen
+    # General
+    gcc
+    g++ 
+    cpp
+    gdb
+    valgrind
+    doxygen
+    binutils
+    coreutils
+    # Python
+    python
+    python-numpy
+    python-matplotlib
+    python-software-properties
+    python3
+    python3-numpy
+    python3-matplotlib
+    python3-software-properties
+    # Misc.
+    lib32ncurses5-dev
+    libncurses5-dev
+    linux-headers-`uname -r`
+)
+
+MISC_DEVELOPMENT_PACKAGES=(
+    # Multimedia
+    libtag1v5
+    libtag1-dev
+    # Misc.
+    pkg-config 
+    flex 
+    bison
+    texinfo
+    doxygen
+    libxml-parser-perl
+    intltool
+    dos2unix
+    fakeroot
+    yasm
+    encfs
+    libtool
+    libglib2.0-dev
+    gperf
+    libc6-dev-i386
+    zlib1g:i386
+    lib32stdc++6
+    xutils-dev
+    xfonts-utils
+    libgl1-mesa-dev
+    dh-modaliases
+    execstack
+    debhelper
+    dkms
+)
+
+#####################################################################
+# Language
+#####################################################################
+LANGUAGES=(
+    anthy
+    anthy-common
+    gijten
+    ibus-anthy
+)
 
 
-# Download Deb Packages
-wget -P ~/Downloads https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-wget -P ~/Downloads http://repo.steampowered.com/steam/archive/precise/steam_latest.deb
-wget -P ~/Downloads http://download.qt.io/official_releases/online_installers/qt-unified-linux-x64-online.run
-
-# Need to update the keys and repository list. When first installing
-# libappindicator1 there was a warning saying the package couldn't be authenticated
-sudo apt-key update
-sudo apt-get update
-
-# Install dependencies needed by Google Chrome. 
-sudo apt-get install -y libgconf2-4 libnss3-1d libxss1 libappindicator1 libindicator7
-
-# Install Chrome, Sublime, Qt Libraries, and Rust
-sudo dpkg -i ~/Downloads/google-chrome-stable_current_amd64.deb
-#sudo dpkg -i ~/Downloads/sublime-text_build-3103_amd64.deb
-
-chmod +x ~/Downloads/qt-unified-linux-x64-online.run
-sudo ~/Downloads/qt-unified-linux-x64-online.run & # Allow installation to be done in 
-                                                      # background.
-                                            
-# Add our repositories
-# The gcc-arm and node.js repo's aren't strictly needed as there are versions in the default repositories.
-# They are being left in in the event that we want to update to newer versions in the future.
-#sudo add-apt-repository ppa:team-gcc-arm-embedded/ppa
-#sudo add-apt-repository -y 'deb http://ppa.launchpad.net/tuxpoldo/btsync/ubuntu vivid main'
-#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D294A752 # Get key for btsync
-#sudo add-apt-repository -y 'deb http://ppa.launchpad.net/chris-lea/node.js/ubuntu utopic main'
-#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C7917B12 # Get key for node.js
-#sudo add-apt-repository -y 'deb http://ppa.launchpad.net/ubuntuhandbook1/birdfont/ubuntu vivid main'
-#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 852541CB # Get key for birdfont 
-#sudo add-apt-repository -y 'deb http://ppa.launchpad.net/daniel.pavel/solaar/ubuntu trusty main'
-#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 06524FBA # Get key for solaar
-sudo add-apt-repository -y 'deb http://download.videolan.org/pub/debian/stable/ /'
-wget -O - http://download.videolan.org/pub/debian/videolan-apt.asc | sudo apt-key add -
-
-
-# Add Syncthing (steps retrieved from apt.syncthing.net)
-# Add the release PGP keys:
-sudo apt-get install curl
-curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
-
-# Add the "release" channel to your APT sources:
-echo "deb http://apt.syncthing.net/ syncthing release" | sudo tee /etc/apt/sources.list.d/syncthing.list
-
-
-
-# Update to the latest
-sudo apt-get update
-sudo apt-get upgrade -y
-
-# Development Stuff
-sudo apt-get install -y --ignore-missing \
-        gcc g++ pkg-config flex bison texinfo doxygen libxml-parser-perl \
-        intltool dos2unix fakeroot yasm encfs autoconf automake libtool \
-        libglib2.0-dev gperf libc6-dev-i386 lib32ncurses5-dev zlib1g:i386 \
-        lib32stdc++6 xutils-dev xfonts-utils gdb valgrind python python3 \
-        doxygen cpp vim binutils coreutils make git exuberant-ctags \
-        libncurses5-dev python-software-properties linux-headers-`uname -r` \
-        screen libgl1-mesa-dev \
-        python3-numpy python-numpy python3-matplotlib python-matplotlib \
-        dh-modaliases execstack debhelper dkms
-
-
-# Install Node.js
-sudo apt-get install -y nodejs
-sudo chown -R $(whoami) ~/.npm
-
-
-# Japanese Stuff
-sudo apt-get install -y --ignore-missing \
-         anthy anthy-common gijten ibus-anthy
-
-
+#####################################################################
 # Utilities
-sudo apt-get install -y --ignore-missing \
-        solaar syncthing openvpn ssh
+#####################################################################
+UTILITIES=(
+    solaar
+    syncthing
+    ssh
+    openvpn
+)
 
+#####################################################################
 # Applications
-sudo apt-get install -y --ignore-missing \
-        vlc picard soundconverter handbrake gimp audacity ardour \
-        inkscape synaptic freecad openscad blender 
+#####################################################################
+APPLICATIONS=(
+    vlc
+    picard
+    soundconverter
+    handbrake
+    gimp
+    inkscape
+    synaptic
+    freecad
+    openscad
+    blender
+)
 
+#####################################################################
+# Multimedia
+#####################################################################
+MULTIMEDIA=(
+    gstreamer1.0-fluendo-mp3
+    gstreamer1.0-plugins-base
+    gstreamer1.0-plugins-good
+    gstreamer1.0-plugins-bad
+    gstreamer1.0-plugins-ugly
+    libdvdcss2
+)
 
+#####################################################################
+# Multimedia
+#####################################################################
+PREREQUISITES=(
+    curl
+)
 
-# Multimedia libraries
-sudo apt-get install -y --ignore-missing \
-        gstreamer0.10-plugins-base gstreamer0.10-plugins-good \
-        gstreamer1.0-fluendo-mp3 \
-        gstreamer1.0-plugins-base gstreamer1.0-plugins-good \
-        gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly libdvdcss2
+function install_prereqs () {
+    echo "install_prereqs"
+    sudo apt-get -y --ignore-missing install ${PREREQUISITES[*]}
+}
 
+function update_repos () {
+    echo "update_repos"
+    install_prereqs
 
+    # vlc
+    wget -O - http://download.videolan.org/pub/debian/videolan-apt.asc | sudo apt-key add -
+    sudo add-apt-repository -y 'deb http://download.videolan.org/pub/debian/stable/ /'
 
+    # syncthing
+    curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
+    echo 'deb http://apt.syncthing.net/ syncthing release' | sudo tee /etc/apt/sources.list.d/syncthing.list
+}
 
-# Remove Scopes
-sudo apt-get remove --purge -y \
-        unity-scope-calculator unity-scope-gdrive unity-scope-yelp \
-        unity-scope-texdoc unity-scope-devhelp unity-scope-virtualbox \
-        unity-scope-audacious unity-scope-clementine unity-scope-tomboy \
-        unity-scope-gmusicbrowser unity-scope-guayadeque unity-scope-manpages \
-        unity-scope-chromiumbookmarks unity-scope-firefoxbookmarks \
-        unity-scope-zotero unity-scope-colourlovers unity-scope-openclipart \
-        unity-scope-musique unity-scope-gourmet unity-scope-musicstores \
-        unity-scope-video-remote
+function install_packages () {
+    echo "install_packages"
+    install_prereqs
 
+    # google-chrome
+    sudo dpkg -i ~/Downloads/google-chrome-stable_current_amd64.deb
+    sudo apt-get -f install
 
-# Remove unwanted lens'
-sudo apt-get remove --purge -y \
-        unity-lens-photos unity-lens-music unity-lens-files
-        
+    # install steam
+    sudo dpkg -i ~/Downloads/steam.deb
+    sudo apt-get -f install
+
+    ALLPACKAGES=(
+        "${DEVELOPMENT_PACKAGES[*]}"
+        "${MULTIMEDIA[*]}"
+        "${APPLICATIONS[*]}"
+        "${UTILITIES[*]}"
+        "${LANGUAGES[*]}"
+    )
+
+    # Setup log file for dumping the 
+    DATETIME=`date +"%Y%m%d_%H%M%S"`
+    INSTALL_LOG_DIR=$SCRIPTROOT/install_packages
+    mkdir -p $INSTALL_LOG_DIR
+    INSTALL_LOG_FILE=$INSTALL_LOG_DIR/$DATETIME.log
+    echo "" > $INSTALL_LOG_FILE
+
+    for i in ${ALLPACKAGES[*]}; do
+        echo "Installing $i..." | tee -a $INSTALL_LOG_FILE
+        echo "--------------------------------------------" | tee -a $INSTALL_LOG_FILE
+        sudo apt-get -y --ignore-missing install $i 2>> $INSTALL_LOG_FILE
+        echo "" >> $INSTALL_LOG_FILE
+    done
+}
+
+function download_packages () {
+    echo "download_packages"
+
+    wget -P ~/Downloads https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    wget -P ~/Downloads https://steamcdn-a.akamaihd.net/client/installer/steam.deb
+
+    # For now, we'll use Qt libraries in the Ubuntu repositories
+    # wget -P ~/Downloads http://download.qt.io/official_releases/online_installers/qt-unified-linux-x64-online.run
+}
+
+function update_configs () {
+    echo "update_configs"
+
+    # Update configs
+    $SCRIPTROOT/configs.sh restore
+    # Update bashrc 
+    sed -i -n -e '1,/#BEGIN_CUSTOM/{ /#BEGIN_CUSTOM/d; p }' -e '/#END_CUSTOM/,${ /#END_CUSTOM/d; p }' ~/.bashrc
+    cat bashrc.custom >> ~/.bashrc
+}
+
+function full () {
+    echo "full"
+    install_prereqs
+    download_packages
+    update_repos
+    install_packages
+    update_configs
+}
+
+# Parameters
+#   -f|--full
+#   -i|--install-packages
+#   -r|--update-repos
+#   -d|--download-packages
+#   -c|--update-configs
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -f|--full)
+    full
+    break # If full, we don't want to re-run everything
+    ;;
+
+    -i|--install-packages)
+    install_packages
+    shift # past argument
+    ;;
+
+    -r|--update-repos)
+    update_repos
+    shift # past argument
+    ;;
+
+    -d|--download-packages)
+    download_packages
+    shift # past argument
+    ;;
+
+    -c|--update-configs)
+    update_configs
+    shift # past argument
+    ;;
+
+    *)    # unknown option
+    shift # past argument
+    ;;
+esac
+done
